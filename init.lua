c523da4dfd4eb41cecd7c9f184060c02b34a4e40
Dennis Rall <dennis.rall@wogra.com>
2024-04-05 08:52:11 +0200

    feat: add custom plugins

--- init.lua
+++ init.lua
@@ -974,6 +974,8 @@ require('lazy').setup({
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
+
+  { import = 'custom.plugins' },
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   -- { import = 'custom.plugins' },
