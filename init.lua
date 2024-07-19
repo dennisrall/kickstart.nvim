a7ade35afe3c48583d41cb4be30b96aad63da303
Dennis Rall <dennis.rall@smart-cybersecurity.de>
2024-07-19 08:28:28 +0200

    feat: enable rust lsp

--- init.lua
+++ init.lua
@@ -628,6 +628,7 @@ require('lazy').setup({
         -- spectral = {},      -- openapi
         pyright = {},
         ruff_lsp = {},
+        rust_analyzer = {}, -- rust
         tailwindcss = {},
         terraformls = {},
         volar = {}, -- vue
@@ -637,7 +638,6 @@ require('lazy').setup({
 
         -- clangd = {},
         -- gopls = {},
-        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
