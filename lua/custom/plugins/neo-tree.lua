33c936a88eee827f38165cf680c7abbcca557968
Dennis Rall <dennis.rall@smart-cybersecurity.de>
2024-08-26 14:43:32 +0200

    feat: update plugins

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -11,7 +11,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
@@ -23,22 +23,3 @@ return {
     },
   },
 }
--- vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
---
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   version = '*',
---   dependencies = {
---     'nvim-lua/plenary.nvim',
---     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
---     'MunifTanjim/nui.nvim',
---   },
---   config = function()
---     require('neo-tree').setup {
---       close_if_last_window = true,
---     }
---
---     vim.keymap.set('n', '<leader>tt', ':Neotree focus<CR>', { desc = 'Open NeoTree', silent = true })
---     vim.keymap.set('n', '<leader>tf', ':Neotree reveal<CR>', { desc = 'Open NeoTree with focus on current file', silent = true })
---   end,
--- }
